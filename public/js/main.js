7bb8522944fee80ad860348b67b6468194eec990
Sarim Khan <sarim.khan0505@gmail.com>
2023-05-29 15:59:43 +0530

    made some API changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,18 @@
+// import * as dotenv from '../../dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+// dotenv.config()
+// import express from '../../express'
+// require('../../dotenv').config()
+
+// const api_key = require("../../src/app")
+// console.log(api_key);
+
 const submitBtn = document.getElementById('submitBtn')
 const cityName = document.getElementById('cityName')
 const city_name = document.getElementById('city_name')
 const temp_real_val = document.getElementById('temp_real_val')
 const temp_status = document.getElementById('temp_status')
 const datahide = document.querySelector('.middle_layer ')
-
+// console.log(process.env);
 const getInfo = async (event) => {
     event.preventDefault()
     const cityVal = cityName.value
@@ -14,7 +22,12 @@ const getInfo = async (event) => {
         datahide.classList.add('data_hide')
     } else {
         try {
-            let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=d40c339aed6a4ca7c0fdad875bb8418e`
+
+            const temp_api = await fetch('http://localhost:8000/api')
+            const third = await temp_api.json()
+            console.log(third.api);
+
+            let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=${third.api}`
             const response = await fetch(url)
             const data = await response.json()
             const arrData = [data]
--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const path = require('path')
 const hbs = require('hbs')
+require('dotenv').config('../.env')
 const app = express()
 const port = 8000
 
+console.log(process.env.API_KEY);
+
+const api_key = process.env.API_KEY
 const static_path = path.join(__dirname, '../public')
 const template_path = path.join(__dirname, '../templates/views')
 const partials_path = path.join(__dirname, '../templates/partials')
@@ -15,8 +19,12 @@ hbs.registerPartials(partials_path)
 
 app.use(express.static(static_path))
 
-app.get("", (req, res) => {
+app.get("/", (req, res) => {
     res.render('index')
+})
+
+app.get("/api", (req, res) => {
+    return res.json({ api: api_key })
 })
 
 app.get("/about", (req, res) => {
@@ -36,3 +44,5 @@ app.get("*", (req, res) => {
 app.listen(port, () => {
     console.log(`listening the port at ${port}`);
 })
+
+module.exports = api_key
